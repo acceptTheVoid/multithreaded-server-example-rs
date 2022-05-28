eaf503bc7044c54ff841d4084c2ce1a401bfb26a
acceptTheVoid <arcad2004@gmail.com>
2022-05-29 08:39:39 +1000

    Fuck i forgor about second chapter

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,29 @@
 
+use multithreaded_server_example_rs as mse;
+
+use mse::ThreadPool;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let ip = "127.0.0.1:7878";
+    let listener = TcpListener::bind(ip).unwrap();
+    let pool = ThreadPool::new(4);
+
+    println!("Server runs at: {}", ip);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -18,8 +31,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
